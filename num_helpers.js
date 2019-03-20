681edbbecb25f91334439f54bc360e47a3c9c49c
Scovia Nnamiggo <2305senk@gmail.com>
2019-03-20 12:01:42 +0300

    creating file n analysing the problem

---
+++ num_helpers.js
@@ -0,0 +1,22 @@
+This assignment shall be called num_helpers.js. Given numbers from 0 to 100, go through all the numbers, while printing out an array that contains what type of number it is, by checking if it belongs to any of the following: (odd, even, prime) And printing out fizz, buzz or fizzbuzz where it applies.
+
+For example,
+
+    at 0, print out: ['even']
+    at 3, print out: ['odd', 'prime', 'fizz']
+    at 30, print out: ['even', 'fizzbuzz']
+
+The catch: We want to see you using helper functions from your earlier assignments for each of those checks / conditions
+
+** Make sure that your code works properly before proceeding to the next step.
+
+Check out into another branch and improve your code so that on top of the previous logic, it also counts the total numbers of each time and prints the totals at the end.
+
+
+For example: It could print,
+
+Even numbers: 20
+
+Odd numbers: 30
+
+Fizz numbers: 26
